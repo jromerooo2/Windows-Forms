2efe20a692ff1dcbfc5114dc55390d34e367cf11
David Andrés Flores Valle <20180422@ricaldone.edu.sv>
2021-04-20 14:20:27 -0600

    gg

--- FastFoodDemo/MySecondCustmControl.cs
+++ FastFoodDemo/MySecondCustmControl.cs
@@ -21,5 +21,15 @@ namespace FastFoodDemo
         {
 
         }
+
+        private void MySecondCustmControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
