74022a17190fb631ab6d6975d7ba71017c64b34e
José Santiago Merino Herrera <20190016@ricaldone.edu.sv>
2021-04-20 14:21:27 -0600

    Cambios del login esteticos: Santi

--- FastFoodDemo/Login.Designer.cs
+++ FastFoodDemo/Login.Designer.cs
@@ -42,9 +42,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(336, 48);
+            this.label1.Location = new System.Drawing.Point(252, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(186, 37);
+            this.label1.Size = new System.Drawing.Size(147, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "Bienvenido!";
             // 
@@ -52,9 +53,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(210, 85);
+            this.label3.Location = new System.Drawing.Point(158, 69);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(493, 28);
+            this.label3.Size = new System.Drawing.Size(381, 22);
             this.label3.TabIndex = 2;
             this.label3.Text = "Inicia sesion para ver nuestros productos";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -64,17 +66,17 @@
             this.panel44.BackColor = System.Drawing.Color.Blue;
             this.panel44.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel44.Location = new System.Drawing.Point(0, 0);
-            this.panel44.Margin = new System.Windows.Forms.Padding(4);
             this.panel44.Name = "panel44";
-            this.panel44.Size = new System.Drawing.Size(857, 12);
+            this.panel44.Size = new System.Drawing.Size(643, 10);
             this.panel44.TabIndex = 3;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(334, 143);
+            this.pictureBox2.Location = new System.Drawing.Point(250, 116);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(177, 145);
+            this.pictureBox2.Size = new System.Drawing.Size(133, 118);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
@@ -83,9 +85,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(171, 339);
+            this.label2.Location = new System.Drawing.Point(272, 265);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 22);
+            this.label2.Size = new System.Drawing.Size(81, 20);
             this.label2.TabIndex = 8;
             this.label2.Text = "Su usuario";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -94,15 +97,16 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 10.8F);
-            this.label4.Location = new System.Drawing.Point(171, 395);
+            this.label4.Location = new System.Drawing.Point(253, 325);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(145, 22);
+            this.label4.Size = new System.Drawing.Size(116, 20);
             this.label4.TabIndex = 9;
             this.label4.Text = "Su contrasenia";
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
@@ -110,8 +114,9 @@
             this.Controls.Add(this.panel44);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Login";
-            this.Size = new System.Drawing.Size(857, 620);
+            this.Size = new System.Drawing.Size(643, 504);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
